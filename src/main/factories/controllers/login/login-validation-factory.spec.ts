6b322672cdba69770dfcf813e3ce1c9d2a0515b4
ThiagoProvi <thiago@provi.com.br>
2021-08-10 07:44:55 -0300

    refactor: separate factories in folders

--- src/main/factories/controllers/login/login-validation-factory.spec.ts
+++ src/main/factories/controllers/login/login-validation-factory.spec.ts
@@ -1,11 +1,11 @@
 import { makeLoginValidation } from './login-validation-factory'
-import { EmailValidation, RequiredFieldValidation } from '../../../presentation/helpers/validators'
+import { EmailValidation, RequiredFieldValidation } from '../../../../presentation/helpers/validators'
 
-import { Validation } from '../../../presentation/protocols/validation'
-import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
-import { EmailValidator } from '../../../presentation/protocols/email-validator'
+import { Validation } from '../../../../presentation/protocols/validation'
+import { ValidationComposite } from '../../../../presentation/helpers/validators/validation-composite'
+import { EmailValidator } from '../../../../presentation/protocols/email-validator'
 
-jest.mock('../../../presentation/helpers/validators/validation-composite')
+jest.mock('../../../../presentation/helpers/validators/validation-composite')
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
