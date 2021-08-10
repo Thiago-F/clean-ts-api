9c0df15a7c80d5002817ca650766e21e9e22dec0
ThiagoProvi <thiago@provi.com.br>
2021-08-10 08:00:01 -0300

    feat: ensure DbAddAccount retuns null if LoadAccountByEmailRepository returns an account

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -8,12 +8,14 @@ export class DbAddAccount implements AddAccount {
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   ) {}
 
-  async add (accountData: AddAccountModel): Promise<AccountModel> {
-    await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
+  async add (accountData: AddAccountModel): Promise<AccountModel | null> {
+    const account = await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
 
-    const hashedPassword = await this.hasher.hash(accountData.password)
-    const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
+    if (!account) {
+      const hashedPassword = await this.hasher.hash(accountData.password)
+      return await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
+    }
 
-    return await new Promise(resolve => resolve(account))
+    return null
   }
 }
